ba20f71095cc8f614c22e52203a82f48a3b2cc77
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-07 12:13:25 +0100

    Adding label to display timer

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,9 +1,12 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Stopwatch implements ActionListener{
 
@@ -16,12 +19,25 @@ public class Stopwatch implements ActionListener{
 	int minutes = 0;
 	int hours = 0;
 	boolean started = false;
-	String seconds_String = String.format("%02d", seconds);
-	String minutes_String = String.format("%02d", minutes);
-	String hourss_String = String.format("%02d", hours);
+	String seconds_string = String.format("%02d", seconds);
+	String minutes_string = String.format("%02d", minutes);
+	String hours_string = String.format("%02d", hours);
 	
 	Stopwatch(){
 		
+		timeLabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
+		timeLabel.setBounds(100,100,200,100);
+		timeLabel.setFont(new Font("Verdana", Font.PLAIN, 35));
+		timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+		timeLabel.setOpaque(true);
+		timeLabel.setHorizontalAlignment(JTextField.CENTER);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.setLayout(null);
+		
+		frame.add(timeLabel);
+		frame.setVisible(true);
 	}
 	
 	@Override
