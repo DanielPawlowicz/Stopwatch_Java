7e5be01a49edc1a32a490c564d397c005cd3d415
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-07 12:07:08 +0100

    Initial classes: main and stopwatch

---
+++ src/Stopwatch.java
@@ -0,0 +1,32 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Stopwatch implements ActionListener{
+
+	Stopwatch(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	void start() {
+		
+	}
+	
+	void stop() {
+		
+	}
+
+	void reset() {
+		
+	}
+	
+}
