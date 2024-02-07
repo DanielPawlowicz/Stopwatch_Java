734e65174e696f08fc7f102836b291b365f0cf9a
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-07 12:07:46 +0100

    Initial variables

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -7,6 +7,19 @@ import javax.swing.JLabel;
 
 public class Stopwatch implements ActionListener{
 
+	JFrame frame = new JFrame();
+	JButton startButton = new JButton("Start");
+	JButton resetButton = new JButton("Reset");
+	JLabel timeLabel = new JLabel();
+	int elapsedTime = 0;
+	int seconds = 0;
+	int minutes = 0;
+	int hours = 0;
+	boolean started = false;
+	String seconds_String = String.format("%02d", seconds);
+	String minutes_String = String.format("%02d", minutes);
+	String hourss_String = String.format("%02d", hours);
+	
 	Stopwatch(){
 		
 	}
